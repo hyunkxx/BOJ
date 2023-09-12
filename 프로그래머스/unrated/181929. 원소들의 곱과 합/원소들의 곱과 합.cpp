6fbaf2b769378154fb9a9502ee1fc4eecc1529d6
Hyunk <103893911+hyunkxx@users.noreply.github.com>
2023-09-13 00:00:21 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int sum1 = 1;
+    int sum2 = 0;
+    
+    for(int i = 0 ; i < num_list.size(); ++i)
+    {
+        sum1 *= num_list[i];
+        sum2 += num_list[i];
+    }
+    
+    return sum1 < sum2 * sum2 ? 1 : 0;
+}
